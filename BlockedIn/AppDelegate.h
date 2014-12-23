25e08dc1b1fa3da58e14420906207024a7cd9ca0
Michael Brew <michael.brew@movial.com>
2014-12-23 14:37:05 -0500

    Initial commit

---
+++ BlockedIn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlockedIn
+//
+//  Created by Michael Brew on 12/23/14.
+//  Copyright (c) 2014 Michael Brew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlockedIn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlockedIn
+//
+//  Created by Michael Brew on 12/23/14.
+//  Copyright (c) 2014 Michael Brew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
